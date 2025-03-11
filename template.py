8a1e7e27ee6e7594210eb4d24cb330720618bab4
hirwa cedric <hirwacedr12@gmail.com>
2025-03-11 19:07:12 +0200

    Update template.py to include prompt.py and remove requirements.txt from the file list

--- template.py
+++ template.py
@@ -7,8 +7,8 @@ logging.basicConfig(level=logging.INFO, format= '[%(asctime)s]:%(message)s')
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
+    "src/prompt.py",
     ".env",
-    "requirements.txt",
     "setup.py",
     "app.py",
     "research/trials.ipynb"                 
