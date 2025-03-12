43d9d8cf1ed9fb09033ca3bb8cf3ef536568d146
hirwa cedric <hirwacedr12@gmail.com>
2025-03-12 21:42:24 +0200

    Add text splitting functionality to helper.py

--- src/helper.py
+++ src/helper.py
@@ -12,6 +12,15 @@ def load_pdf(data):
 
     return documents
 
+
+#Create text chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
+
+
 #download embedding model
 def download_hugging_face_embeddings():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
