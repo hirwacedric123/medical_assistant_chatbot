527bdc79debb381d2654617f747c0e0639538b2f
hirwa cedric <hirwacedr12@gmail.com>
2025-03-12 10:26:34 +0200

    Add PDF loading function to helper.py for document processing

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,14 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
+
+
+#Extract data from the PDF
+def load_pdf(data):
+    loader = DirectoryLoader(data,
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    
+    documents = loader.load()
+
+    return documents
