98ba56634fdd2a741dd043f1d64f7f7cb3540a0f
hirwa cedric <hirwacedr12@gmail.com>
2025-03-12 10:27:28 +0200

    Add text splitting function to helper.py for processing extracted PDF data

--- src/helper.py
+++ src/helper.py
@@ -12,3 +12,11 @@ def load_pdf(data):
     documents = loader.load()
 
     return documents
+
+
+#Create text chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
