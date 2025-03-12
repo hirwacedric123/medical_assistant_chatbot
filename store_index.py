5bb3da1869b91c807b322bf0eb6b826ebd0a8b76
hirwa cedric <hirwacedr12@gmail.com>
2025-03-13 00:09:13 +0200

    Initialize Pinecone with API key and set index name in store_index.py

--- store_index.py
+++ store_index.py
@@ -17,13 +17,10 @@ extracted_data = load_pdf("data/")
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
-
-#Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "medicalbot"
 
 
-index_name="medicalbot"
 
 #Creating Embeddings for Each of The Text Chunks & storing
 docsearch= PineconeVectorStore.from_documents(
